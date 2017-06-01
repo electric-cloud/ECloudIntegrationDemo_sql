3cf16fc7f6a82d3a3c95871e42cf49a87f156eee
Laurent Rochette <lrochette@gmail.com>
2017-06-01 15:48:05 -0500

    change comment to force build

---
+++ sql_code/ddl/create-table-categories.sql
@@ -0,0 +1,16 @@
+/****** Object:  Table [dbo].[Categories]    Script Date: 06/01/2017 15:47:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](15) NOT NULL,
+	[Description] [ntext] NULL,
+	[Picture] [image] NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
