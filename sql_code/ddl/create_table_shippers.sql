0ae1ebe7b192f8189f131671db4fa8deb7a3e83c
Laurent Rochette <lrochette@gmail.com>
2017-06-02 10:47:06 -0500

    New table shippers

---
+++ sql_code/ddl/create_table_shippers.sql
@@ -0,0 +1,15 @@
+/****** Object:  Table [dbo].[Shippers]    Script Date: 08/12/2011 11:46:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Shippers](
+	[ShipperID] [int] IDENTITY(1,1) NOT NULL,
+	[CompanyName] [nvarchar](40) NOT NULL,
+	[Phone] [nvarchar](24) NULL,
+ CONSTRAINT [PK_Shippers] PRIMARY KEY CLUSTERED
+(
+	[ShipperID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
