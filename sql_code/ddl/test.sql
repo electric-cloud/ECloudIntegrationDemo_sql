651917deaf95dba27a0922e3a50f9012b8077cf3
Laurent Rochette <lrochette@gmail.com>
2017-06-08 16:02:25 -0500

    turn identity insert to ON [utf8]

--- sql_code/ddl/test.sql
+++ sql_code/ddl/test.sql
@@ -12,6 +12,6 @@ CREATE TABLE [dbo].[francais](
   )
 )
 GO
-
+SET IDENTITY_INSERT [dbo].[francais] ON
 INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (1, 'Rochette', 'laurent')
 INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (2, 'Chabot', 'Geneviève')
