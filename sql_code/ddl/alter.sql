d047f9ba6525d38d9ae621e2d770b9fd285c0f5c
Laurent Rochette <lrochette@gmail.com>
2017-06-09 08:44:17 -0500

    Alter table [utf8]

---
+++ sql_code/ddl/alter.sql
@@ -0,0 +1,8 @@
+/* setlocale (LC_ALL, 'nl_BE');*/
+sp_rename 'dbo.francais.prénom', 'prenom', 'COLUMN';
+
+/*
+SET IDENTITY_INSERT [dbo].[francais] ON
+INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (1, 'Rochette', 'laurent')
+INSERT [dbo].[francais] ([ID], [nom], [prénom]) VALUES (2, 'Chabot', 'Geneviève')
+*/
